884476f6cc56ae075cf44b3c46f1781fbe75d574
yrahull595 <114355514+yrahull595@users.noreply.github.com>
2025-02-08 19:03:40 +0530

    VUEX Practice

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import Vue from 'vue'
+
+import { createApp } from 'vue'
 import App from './App.vue'
+import store from './store'
 
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+const app = createApp(App)
+app.use(store)
+app.mount('#app')
